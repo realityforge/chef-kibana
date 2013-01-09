a92090d7e6549fc15aebe21c90edc96eb3effe0c
Peter Donald <peter@realityforge.org>
2013-01-09 13:41:26 +1100

    Bump version for release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'peter@realityforge.org'
 license          'Apache 2.0'
 description      'Installs/Configures kibana, the logstash UI'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.2'
 
 suggests 'authbind'
 suggests 'apache2'
