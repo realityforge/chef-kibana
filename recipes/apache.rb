087c37bce2fdfa9289dff4e05a30f2a3fcd85a69
Peter Donald <peter@realityforge.org>
2013-04-22 10:16:11 +1000

    Stop declaraing the apache template twice.

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -3,11 +3,6 @@ include_recipe 'apache2::mod_proxy_http'
 
 template "#{node['apache']['dir']}/sites-available/kibana" do
   source 'vhost.conf.erb'
-  notifies :restart, 'service[apache2]'
-end
-
-template "#{node['apache']['dir']}/sites-available/kibana" do
-  source 'vhost.conf.erb'
   owner node['apache']['user']
   group node['apache']['group']
   mode 00644
