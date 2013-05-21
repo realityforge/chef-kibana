be4f7a444e5abee7057a2bca824383b3695edc5c
Ryan Richard <ryanrichard07@gmail.com>
2013-05-21 16:13:34 -0500

    update ruby version logic

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'peter@realityforge.org'
 license          'Apache 2.0'
 description      'Installs/Configures kibana, the logstash UI'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
 
 depends 'build-essential'
 suggests 'authbind'
 suggests 'apache2'
-
+suggests 'apt'
