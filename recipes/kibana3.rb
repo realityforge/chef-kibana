dcd8c05d7e430bdd92088a2f38956b5f8c44f372
Gregoire Seux <g.seux@criteo.com>
2014-01-13 11:13:43 +0100

    Use port attribute when using kibana3

--- recipes/kibana3.rb
+++ recipes/kibana3.rb
@@ -28,4 +28,7 @@ template File.join(node['kibana']['base_dir'], config_path) do
   owner node['kibana']['user']
   group node['kibana']['group']
   mode '0644'
+  variables({
+    'es_port' => node['kibaka']['elasticsearch']['port'],
+  })
 end
