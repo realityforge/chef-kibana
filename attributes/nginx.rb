fd3f3d0bbdf6d456b188544485e04f58b76fb8f3
Jonathon W. Marshall <jonathon@bnotions.com>
2014-09-18 01:09:04 -0400

    update ciphers and protocols

--- attributes/nginx.rb
+++ attributes/nginx.rb
@@ -11,8 +11,8 @@ default['kibana']['nginx']['ssl_certificate']     = 'nil'
 default['kibana']['nginx']['ssl_certificate_key'] = 'nil'
 
 # nginx ssl boilerplate
-default['kibana']['nginx']['ssl_protocols']       = 'SSLv3 TLSv1 TLSv1.1 TLSv1.2'
-default['kibana']['nginx']['ssl_ciphers']         = 'HIGH:!aNULL:!MD5:!EXP:!kEDH'
+default['kibana']['nginx']['ssl_protocols']       = 'TLSv1 TLSv1.1 TLSv1.2'
+default['kibana']['nginx']['ssl_ciphers']         = 'ECDHE-RSA-RC4-SHA:ECDHE-RSA-AES128-SHA:AES256-CGM-SHA256:ECDHE-RSA-AES256-SHA256:RC4:HIGH:!aNULL:!MD5:-LOW:-SSLv2:-EXP'
 default['kibana']['nginx']['ssl_session_cache']   = 'shared:SSL:10m'
 default['kibana']['nginx']['ssl_session_timeout'] = '10m'
 
