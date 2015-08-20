3a3c927cf290a5fb6335b11b4329c1e983ce001a
Peter Donald <peter@realityforge.org>
2015-08-21 08:37:55 +1000

    Whitespace

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
 # Encoding: utf-8
 
-name             'kibana'
-maintainer       'Peter Donald'
+name 'kibana'
+maintainer 'Peter Donald'
 maintainer_email 'peter@realityforge.org'
-license          'Apache 2.0'
-description      'Installs/Configures kibana, the logstash UI'
+license 'Apache 2.0'
+description 'Installs/Configures kibana, the logstash UI'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.9'
+version 0.1.9'
 
 depends 'build-essential'
 depends 'ark'
