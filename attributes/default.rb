71ae7e3f632f84e8b370013f2b2c3b60e662e78c
Ihor Bilovus <barisik@outlook.com>
2015-11-02 18:41:52 +0200

    Bump kibana version to 4.2.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@ default['kibana']['version'] = '2'
 #<> Kibana3 exact version
 default['kibana']['kibana3_version'] = '3.0.0'
 #<> Kibana4 exact version
-default['kibana']['kibana4_version'] = '4.1.2'
+default['kibana']['kibana4_version'] = '4.2.0'
 #<> The base directory of kibana.
 default['kibana']['base_dir'] = '/opt/kibana'
 #<> The user under which Kibana is installed.
@@ -21,7 +21,7 @@ default['kibana']['url'] = Kibana::Url.new(node, url_version).get
 #<> Checksum of the tarball
 default['kibana']['checksum'] = 'df25bc0cc02385edcac446ef8cbd83b896cdc910a0fa1b0a7bd2a958164593a8'
 #<> Checksum of the tarball (for Kibana4)
-default['kibana']['kibana4_checksum'] = '5f6213f7ac7ef71016a6750f09e7316ccc9bca139bc5389b417395b179bc370c'
+default['kibana']['kibana4_checksum'] = '67d586e43a35652adeb6780eaa785d3d785ce60cc74fbf3b6a9a53b753c8f985'
 
 #<> The URL to Kibana repository.
 default['kibana']['git']['url'] = if node['kibana']['version'] > '2'
