f1b4006ff9f7e86d14e7062a19a15bd8fcf9ba94
Scott Nelson Windels <scott.windels@rackspace.com>
2016-07-12 20:42:59 -0700

    Update serverspec tests to handle case where rhel variants don't have init script, update Berksfile to pull in nginx v2.7.6 and update to supermarket url, update kitchen yaml file to address issue where yum-epel won't load metalink.

--- test/integration/kibana4_nginx/serverspec/default_spec.rb
+++ test/integration/kibana4_nginx/serverspec/default_spec.rb
@@ -5,9 +5,15 @@ describe 'kibana' do
   describe file('/opt/kibana') do
     it { should be_directory }
   end
-  describe service('kibana') do
+  describe service('kibana'), :if => os[:family] == 'debian' do
     it { should be_enabled }
+    it { should be_running }
   end
+# TODO: add support for a rhel service init script
+#  describe service('kibana'), :if => os[:family] == 'rhel' do
+#    it { should be_enabled }
+#    it { should be_running }
+#  end
 end
 
 describe 'nginx' do
