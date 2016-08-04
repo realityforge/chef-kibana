0ee11c06e9755987da601c90b6747ee1b6b9f3f5
Scott Nelson Windels <scott.windels@rackspace.com>
2016-08-04 15:00:50 -0700

    Remove redundant integration test for kibana enabled

--- test/integration/kibana4_nginx/serverspec/default_spec.rb
+++ test/integration/kibana4_nginx/serverspec/default_spec.rb
@@ -5,14 +5,10 @@ describe 'kibana' do
   describe file('/opt/kibana') do
     it { should be_directory }
   end
-  describe service('kibana'), if: os[:family] == 'ubuntu' do
+  describe service('kibana') do
     it { should be_enabled }
     # TODO: This test fails in Travis but was working with a local kitchen verify
     #    it { should be_running }
-  end
-  describe service('kibana'), if: os[:family] == 'redhat' do
-    it { should be_enabled }
-    it { should be_running }
   end
 end
 
