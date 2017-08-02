c78df1f49ff266ddfd5f822e4d8cd59f07da3516
Ramez Hanna <r.hanna@criteo.com>
2017-08-02 18:44:26 +0200

    remove plugin install in favor of https://github.com/realityforge/chef-kibana/pull/106

--- recipes/kibana5.rb
+++ recipes/kibana5.rb
@@ -55,13 +55,3 @@ template File.join(node['kibana']['base_dir'], config_path) do
   )
   notifies :restart, 'service[kibana]'
 end
-
-# Install plugins
-node['kibana']['plugins'].each do |plugin|
-  execute "install #{plugin[:name]}" do
-    command "/usr/local/kibana-#{node['kibana']['kibana5_version']}/bin/kibana-plugin install #{plugin[:url]}"
-    user node['kibana']['user'] || 'root'
-    not_if { ::Dir.exist?("/usr/local/kibana-#{node['kibana']['kibana5_version']}/plugins/#{plugin[:name]}") }
-    notifies :restart, 'service[kibana]'
-  end
-end
