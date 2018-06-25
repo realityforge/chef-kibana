bcef61caf8d3e8d15ed494462ff85a2c965c1e04
Cyberflow <dmitry.r@cyberflow.net>
2018-06-25 10:56:19 +0300

    fix test for kibana5 tar

--- test/integration/kibana5_nginx/controls/default.rb
+++ test/integration/kibana5_nginx/controls/default.rb
@@ -3,8 +3,11 @@ control 'kibana' do
   title 'Test configuration and install kibana'
   desc 'Test configuration and install kibana'
 
-  describe file('/opt/kibana') do
+  describe file('/usr/local/kibana-5.6.4') do
     it { should be_directory }
+  end
+  describe file('/usr/share/kibana/current') do
+    it { should be_symlink }
   end
 end
 
